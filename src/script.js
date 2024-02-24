19bec3827c05b72d5ca437828b87236e1390b2d2
Roman Gudz <alien31roman@gmail.com>
2024-02-24 20:50:19 +0300

    module_04_lessons_03

--- src/script.js
+++ src/script.js
@@ -1,2 +1,22 @@
 import './index.html';
 import './scss/index.scss';
+
+class NewTask {
+  #id;
+  #name;
+  #counter;
+  constructor(id, name, counter = 0) {
+    this.#id = id;
+    this.#name = name;
+    this.#counter = counter;
+  }
+
+  editName(name) {
+    return this.#name = name;
+  }
+
+  editCounter(number = 1) {
+    return this.#counter += number;
+  }
+}
+
